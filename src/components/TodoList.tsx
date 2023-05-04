2b776e9646317a7ebfb2f220be49ed31dd348f7c
Rodley Orosa <rodleyorosa@gmail.com>
2023-05-04 15:01:50 +0200

    fix accordion by using useNavigate hook

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,6 @@
 import { Accordion, Container, Divider, Icon, ListItem, ListV2, Padding, Radio, Row, Text } from "@zextras/carbonio-design-system"
 import { useMemo } from "react"
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 interface Subtask {
     id: string;
@@ -22,30 +22,28 @@ interface TodoListProps {
 
 export const TodoList: React.FC<TodoListProps> = ({ todos }) => {
 
+    const navigate = useNavigate()
+
     const items = useMemo(
         () =>
             todos.map((todo) => (
                 <ListItem key={todo.id}>
                     {() => (
                         <>
-                            <Link to={`/todos/${todo.id}`} style={{ textDecoration: 'none' }}>
-                                <Container orientation="horizontal" mainAlignment="space-between" padding="medium">
-                                    <Accordion activeId={"1"} items={[
-                                        {
-                                            id: todo.id,
-                                            label: todo.id + '. ' + todo.label,
-                                            icon: todo.isCompleted ? "Checkmark" : "",
-                                            badgeType: 'unread',
-                                            badgeCounter: todo.items.length ? todo.items.length : undefined,
-                                            items: todo.items.map((subtask) => ({
-                                                id: subtask.id,
-                                                label: subtask.label
-                                            }))
-                                        }
-                                    ]} />
-                                    <Icon icon={"ArrowIosForwardOutline"} size={"large"} />
-                                </Container>
-                            </Link>
+                            <Accordion padding={"medium"} items={[
+                                {
+                                    id: todo.id,
+                                    label: todo.id + '. ' + todo.label,
+                                    icon: todo.isCompleted ? "Checkmark" : "",
+                                    badgeType: 'unread',
+                                    badgeCounter: todo.items.length ? todo.items.length : undefined,
+                                    onClick: () => navigate("/todos/" + todo.id),
+                                    items: todo.items.map((subtask) => ({
+                                        id: subtask.id,
+                                        label: subtask.label
+                                    }))
+                                }
+                            ]} />
                             <Divider style={{ width: "95%", margin: "auto" }} />
                         </>
                     )}
